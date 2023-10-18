8498d65ef4656379c990d3a4d0c0a489815be3be
Clara Mrkos <mrkos.clara@gmail.com>
2023-10-18 12:15:52 +0200

    Set up login page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -13,22 +13,42 @@ export default function LoginPage() {
         <a href="/#">Forgot your Password?</a>
       </div>
       <div id="SignUpSection">
-        <form id="signUpMenteeOrMentor">
-          <legend>Sign Up</legend>
-          <button id="showMentorSignupForm">
-            As a Mentor: Help someone to get into their dream uni
-          </button>
-          <button id="showMenteeSignupForm">
-            As a Mentee: Get help to get into your dream uni
-          </button>
-        </form>
         <form id="signUpForm">
+          <legend>Sign Up as:</legend>
+          <label htmlFor="mentor">
+            A Mentor: Help someone to get into their dream uni
+          </label>
+          <input
+            type="radio"
+            id="mentor"
+            name="signup_as"
+            value="incomplete_mentor"
+          />
+          <label htmlFor="mentee">
+            A Mentee: Get help to get into your dream uni
+          </label>
+          <input
+            type="radio"
+            id="mentee"
+            name="signup_as"
+            value="incomplete_mentee"
+          />
+          <br />
+
           <label htmlFor="emailInput">Email:</label>
           <input id="emailInput" required />
+
           <label htmlFor="passwordInput">Password:</label>
           <input id="passwordInput" required />
-          <button id="signInButtonMentor">Sign up as a Mentor</button>
-          <button id="signInButtonMentee">Sign up as a Mentee</button>
+          <label htmlFor="passwordInput">Password:</label>
+          <input id="passwordInput" required />
+          <button
+            id="signInButtonMentor"
+            // Button text changes depending on radio button input
+            // creates a new user with role "incomplete mentor" or "incomplete mentee" and redirects to profile input page
+          >
+            Sign up as a Mentor
+          </button>
         </form>
       </div>
     </main>
