ae9765a3661e4a9d43e81c0844d32eaa98cf75d1
Clara Mrkos <mrkos.clara@gmail.com>
2023-11-03 16:02:53 +0100

    Finalize unifinished Matching Algorythm

--- util/matchingAlgorythm.tsx
+++ util/matchingAlgorythm.tsx
@@ -42,81 +42,110 @@ export default async function MatchingAlgorythm(props: Props) {
 
   // filter mentors to those that do not contain the mentees user id in their "rejected" or "past mentors" column - later
 
-  // map  through menteor unilist
+  // check matches of mentor background anad mentee input and assign true and false
+
+  const matchingArray = [
+    {
+      menteeUserId: 0,
+      mentorUserId: 0,
+      studylevelMatch: 0,
+      originCountryMatch: 0,
+      menteeUniOneMatch: 0,
+      menteeUniTwoMatch: 0,
+      menteeUniThreeMatch: 0,
+      menteeSubjectOneMatch: 0,
+      menteeSubjectTwoMatch: 0,
+      menteeSubjectThreeMatch: 0,
+      finalSum: 0,
+    },
+  ];
 
   const mentorTest = confirmedMentors.forEach((element) => {
     const matchingTest = {
       menteeUserId: 0,
       mentorUserId: 0,
-      studylevelMatch: false,
-      originCountryMatch: false,
-      menteeUniOneMatch: false,
-      menteeUniTwoMatch: false,
-      menteeUniThreeMatch: false,
-      menteeSubjectOneMatch: false,
-      menteeSubjectTwoMatch: false,
-      menteeSubjectThreeMatch: false,
+      studylevelMatch: 0,
+      originCountryMatch: 0,
+      menteeUniOneMatch: 0,
+      menteeUniTwoMatch: 0,
+      menteeUniThreeMatch: 0,
+      menteeSubjectOneMatch: 0,
+      menteeSubjectTwoMatch: 0,
+      menteeSubjectThreeMatch: 0,
+      finalSum: 0,
     };
-    const matchingArray = [
-      {
-        menteeUserId: 0,
-        mentorUserId: 0,
-        studylevelMatch: false,
-        originCountryMatch: false,
-        menteeUniOneMatch: false,
-        menteeUniTwoMatch: false,
-        menteeUniThreeMatch: false,
-        menteeSubjectOneMatch: false,
-        menteeSubjectTwoMatch: false,
-        menteeSubjectThreeMatch: false,
-      },
-    ];
+
+    // define and assign weights
+    const weights = {
+      universityMatchWeight: 30,
+      countryMatchWeight: 15,
+      studylevelMatchWeight: 10,
+      subjectMatchWeight: 40,
+    };
+
     matchingTest.mentorUserId = element.usersId;
     matchingTest.menteeUserId = Number(userWithUniversityApplication?.usersId);
 
     if (
       element.usersCountryId === userWithUniversityApplication?.usersCountryId
     ) {
-      matchingTest.originCountryMatch = true;
+      matchingTest.originCountryMatch = weights.countryMatchWeight;
     }
     element.userMentorUniversityBackgrounds?.forEach((element) => {
       if (
         element.universityId ===
         userUniversityApplicationOnly?.firstUniversityId
       ) {
-        matchingTest.menteeUniOneMatch = true;
+        matchingTest.menteeUniOneMatch = weights.universityMatchWeight;
       }
       if (
         element.universityId ===
         userUniversityApplicationOnly?.secondUniversityId
       ) {
-        matchingTest.menteeUniTwoMatch = true;
+        matchingTest.menteeUniTwoMatch = weights.universityMatchWeight;
       }
       if (
         element.universityId ===
         userUniversityApplicationOnly?.thirdUniversityId
       ) {
-        matchingTest.menteeUniThreeMatch = true;
+        matchingTest.menteeUniThreeMatch = weights.universityMatchWeight;
       }
       if (element.subjectId === userUniversityApplicationOnly?.firstSubjectId) {
-        matchingTest.menteeSubjectOneMatch = true;
+        matchingTest.menteeSubjectOneMatch = weights.subjectMatchWeight;
       }
       if (
         element.subjectId === userUniversityApplicationOnly?.secondSubjectId
       ) {
-        matchingTest.menteeSubjectTwoMatch = true;
+        matchingTest.menteeSubjectTwoMatch = weights.subjectMatchWeight;
       }
       if (element.subjectId === userUniversityApplicationOnly?.thirdSubjectId) {
-        matchingTest.menteeSubjectThreeMatch = true;
+        matchingTest.menteeSubjectThreeMatch = weights.subjectMatchWeight;
       }
+      // something isnt working correctly here yet!
       if (
         Number(element.studylevel) ===
         Number(userUniversityApplicationOnly?.studylevel)
       ) {
-        matchingTest.studylevelMatch = true;
+        matchingTest.studylevelMatch = weights.studylevelMatchWeight;
       }
     });
+    matchingTest.finalSum =
+      matchingTest.studylevelMatch +
+      matchingTest.originCountryMatch +
+      matchingTest.menteeUniOneMatch +
+      matchingTest.menteeUniTwoMatch +
+      matchingTest.menteeUniThreeMatch +
+      matchingTest.menteeSubjectOneMatch +
+      matchingTest.menteeSubjectTwoMatch +
+      matchingTest.menteeSubjectThreeMatch;
     matchingArray.push(matchingTest);
-    console.log(matchingTest);
+
+    return matchingArray;
   });
+  console.log(matchingArray);
+
+  const sortedArray = matchingArray.sort(function (a, b) {
+    return Number(a.finalSum) - Number(b.finalSum);
+  });
+  console.log(sortedArray);
 }
