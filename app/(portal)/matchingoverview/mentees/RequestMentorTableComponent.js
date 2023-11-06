9758e70960411c7d16f24951f1cd5a7cd7addab5
Clara Mrkos <mrkos.clara@gmail.com>
2023-11-06 16:36:20 +0100

    Update send request component

--- app/(portal)/matchingoverview/mentees/RequestMentorTableComponent.js
+++ app/(portal)/matchingoverview/mentees/RequestMentorTableComponent.js
@@ -4,21 +4,38 @@ import { useState } from 'react';
 
 export default function RequestMentorTableComponent(props) {
   const [mentorSelection, setMentorSelection] = useState(0);
-  const mentorFirstName = props.mentorUserDataWithUniInfoObjectROWFirstName;
-  const mentorId = props.mentorUserDataWithUniInfoObjectROWId;
-  console.log(mentorSelection);
+  const [
+    topThreeMentorsWithPersonalDataList,
+    setTopThreeMentorsWithPersonalDataList,
+  ] = useState(props.topThreeMentorsWithPersonalDataList);
+
+  console.log(topThreeMentorsWithPersonalDataList);
+
   return (
-    <div className="form-control">
-      <label className="label cursor-pointer">
-        <input
-          className="radio"
-          type="radio"
-          id={`mentor-${mentorId}`}
-          name="requestMentor"
-          value={mentorId}
-          onChange={(event) => setMentorSelection(event.currentTarget.value)}
-        />
-      </label>
-    </div>
+    <>
+      {topThreeMentorsWithPersonalDataList.map((d) => {
+        return (
+          <div key={`uniqueID-${d.id}`}>
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <input
+                  className="radio"
+                  type="radio"
+                  id={`mentor-${d.id}}`}
+                  name="requestMentor"
+                  value={mentorSelection}
+                  onChange={(event) =>
+                    setMentorSelection(event.currentTarget.value)
+                  }
+                />
+              </label>
+            </div>
+            <div>
+              <div className="font-bold">{d.firstname}</div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
