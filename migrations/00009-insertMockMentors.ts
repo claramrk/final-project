1d6927754ccbfcc116259b6a03538ed12e7fd31e
Clara Mrkos <mrkos.clara@gmail.com>
2023-11-13 16:08:34 +0100

    Try grid for matching mentor results

--- migrations/00009-insertMockMentors.ts
+++ migrations/00009-insertMockMentors.ts
@@ -13,6 +13,8 @@ const mockMentorslist = [
     country_id: 'USA',
     max_capacity: 5,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885976/of8nti8lmn87ockbx0nk.png',
   },
   {
     email: 'alice.smith@example.com',
@@ -26,6 +28,8 @@ const mockMentorslist = [
     country_id: 'CAN',
     max_capacity: 8,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885974/wvhiakvc9ukwlxl7kzg1.png',
   },
   {
     email: 'megan.johnson@example.com',
@@ -39,6 +43,8 @@ const mockMentorslist = [
     country_id: 'GBR',
     max_capacity: 6,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885968/p7yso8olwvoklrjagn5t.png',
   },
   {
     email: 'david.brown@example.com',
@@ -52,6 +58,8 @@ const mockMentorslist = [
     country_id: 'AUS',
     max_capacity: 7,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885974/fgukgthstj7ceifqjj1h.png',
   },
   {
     email: 'linda.wilson@example.com',
@@ -65,6 +73,8 @@ const mockMentorslist = [
     country_id: 'FRA',
     max_capacity: 5,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885967/uzzcmnf56svdxnjpebwu.png',
   },
   {
     email: 'william.jones@example.com',
@@ -78,6 +88,8 @@ const mockMentorslist = [
     country_id: 'CAN',
     max_capacity: 8,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885969/b0toqwebyqnzfbmxfi88.png',
   },
   {
     email: 'emily.davis@example.com',
@@ -91,6 +103,8 @@ const mockMentorslist = [
     country_id: 'GBR',
     max_capacity: 6,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699886343/ubyymdkchh8qn1hgkhim.png',
   },
   {
     email: 'james.wilson@example.com',
@@ -104,6 +118,8 @@ const mockMentorslist = [
     country_id: 'AUS',
     max_capacity: 7,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699885969/walrhh1zb0xukku54jk9.png',
   },
   {
     email: 'sarah.miller@example.com',
@@ -117,24 +133,52 @@ const mockMentorslist = [
     country_id: 'FRA',
     max_capacity: 5,
     role_id: 3,
+    photo:
+      'https://res.cloudinary.com/dqmhbukkm/image/upload/v1699886343/fbvuw80xjhb60is8ayfr.png',
   },
 ];
 
 export async function up(sql: Sql) {
   for (const user of mockMentorslist) {
     await sql`
-  INSERT INTO users (
-    email, password_hash, firstname, lastname, pronouns, phone_number, birthdate, country_id,  max_capacity, role_id)
-    VALUES
-    (${user.email}, ${user.password_hash}, ${user.firstname}, ${user.lastname}, ${user.pronouns}, ${user.phone_number}, ${user.birthdate}, ${user.country_id}, ${user.max_capacity}, ${user.role_id})
-  `;
+      INSERT INTO
+        users (
+          email,
+          password_hash,
+          firstname,
+          lastname,
+          pronouns,
+          phone_number,
+          birthdate,
+          country_id,
+          max_capacity,
+          role_id,
+          photo
+        )
+      VALUES
+        (
+          ${user.email},
+          ${user.password_hash},
+          ${user.firstname},
+          ${user.lastname},
+          ${user.pronouns},
+          ${user.phone_number},
+          ${user.birthdate},
+          ${user.country_id},
+          ${user.max_capacity},
+          ${user.role_id},
+          ${user.photo}
+        )
+    `;
   }
 }
 
 export async function down(sql: Sql) {
   for (const user of mockMentorslist) {
     await sql`
-      DELETE FROM users WHERE email = ${user.email}
-`;
+      DELETE FROM users
+      WHERE
+        email = ${user.email}
+    `;
   }
 }
